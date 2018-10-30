ab22b356cbe9fad7ca587e5b9cb37dd5224307de
ke2xin <43608211+ke2xin@users.noreply.github.com>
2018-10-30 18:25:35 +0800

    Initial commit

---
+++ kz/src/main/java/pc/kz/as/kz/KzApplication.java
@@ -0,0 +1,12 @@
+package pc.kz.as.kz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KzApplication.class, args);
+    }
+}
